8cc4b70441e6a428d28382b8355ec9ce5b6f9dcc
Nursultan <nurulla_nursultan@mail.ru>
2024-08-23 21:32:11 +0500

    add script file and js code

---
+++ src/js/script.js
@@ -0,0 +1,49 @@
+var name = "Ivan"; 
+let age = 21; 
+const pi = 12.7;
+
+let obj = {
+
+    name: "apple",
+    color: "green",
+    weight: 200
+
+}
+
+// alert(123)
+
+// console.log("Hello wrold") на консолье надпис Привет Мир 
+
+// confirm("Вам есть 18?") спрашивает у пользователья есть ли унего 18 
+
+// let answer = confirm("Вам есть 18?"); спрашивает у пользователья есть ли унего 18 
+// console.log(answer); Если ответ да то на консолье показывает true а если нет то false 
+
+// console.log(4+4);
+
+// if (2*5 == 8*1){
+//     console.log("Верно")
+// }else {
+//     console.log("Ошибка")
+// }
+
+
+// let answer = confirm("Вам есть 18?"); 
+// if (answer) {
+//     console.log("Проходите")
+// } else {
+//     console.log("Уходите")
+// }
+
+
+// for (let i = 1; i < 8; i++){
+//     console.log(i);
+// }
+
+function logging(a, b) {
+    console.log(a+b)
+}
+
+logging(10, 20)
+
+logging(2,13)
