bde038d44cb1068215611725c4ebf6a33b59c82f
Nursultan <nurulla_nursultan@mail.ru>
2024-08-25 13:30:54 +0500

    add slick slide

--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,41 @@
-var name = "Ivan"; 
-let age = 21; 
-const pi = 12.7;
-
-let obj = {
-
-    name: "apple",
-    color: "green",
-    weight: 200
-
-}
-
-// alert(123)
-
-// console.log("Hello wrold") на консолье надпис Привет Мир 
-
-// confirm("Вам есть 18?") спрашивает у пользователья есть ли унего 18 
-
-// let answer = confirm("Вам есть 18?"); спрашивает у пользователья есть ли унего 18 
-// console.log(answer); Если ответ да то на консолье показывает true а если нет то false 
-
-// console.log(4+4);
-
-// if (2*5 == 8*1){
-//     console.log("Верно")
-// }else {
-//     console.log("Ошибка")
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/leftarrow.png" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/rightarrow.png" alt="arrow"></button>'
+    });
+  });
+// var name = "Ivan"; 
+// let age = 21; 
+// const pi = 12.7;
+// let obj = {
+//     name: "apple",
+//     color: "green",
+//     weight: 200 
 // }
-
-
-// let answer = confirm("Вам есть 18?"); 
-// if (answer) {
-//     console.log("Проходите")
-// } else {
-//     console.log("Уходите")
+// // alert(123)
+// // console.log("Hello wrold") на консолье надпис Привет Мир 
+// // confirm("Вам есть 18?") спрашивает у пользователья есть ли унего 18 
+// // let answer = confirm("Вам есть 18?"); спрашивает у пользователья есть ли унего 18 
+// // console.log(answer); Если ответ да то на консолье показывает true а если нет то false 
+// // console.log(4+4);
+// // if (2*5 == 8*1){
+// //     console.log("Верно")
+// // }else {
+// //     console.log("Ошибка")
+// // }
+// // let answer = confirm("Вам есть 18?"); 
+// // if (answer) {
+// //     console.log("Проходите")
+// // } else {
+// //     console.log("Уходите")
+// // }
+// // for (let i = 1; i < 8; i++){
+// //     console.log(i);
+// // }
+// function logging(a, b) {
+//     console.log(a+b)
 // }
-
-
-// for (let i = 1; i < 8; i++){
-//     console.log(i);
-// }
-
-function logging(a, b) {
-    console.log(a+b)
-}
-
-logging(10, 20)
-
-logging(2,13)
+// logging(10, 20)
+// logging(2,13)
